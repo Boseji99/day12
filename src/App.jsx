e52a207eab84228e4f56a66f3a41f35cd073abf4
Sumon Bose <boseji99@gamil.com>
2025-02-02 17:11:48 +0530

    modified:   src/App.jsx 	new file:   src/components/ChildComponent.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState } from "react";
+import ChildComponent from "./components/ChildComponent";
 
 const App = () => {
+  const [name, setName] = useState("Sumon");
+  const [isToggle, setToggle] = useState(true);
+  const changeparentdata = () => {
+    if (isToggle) {
+      setName("Sumon changed");
+      setToggle(!isToggle);
+    } else {
+      setName("Sumon");
+      setToggle(!isToggle);
+    }
+  };
   return (
     <div>
-      my app
-    </div>
-  )
-}
+      <h1 className="text-center font-bold text-4xl">My Portfolio</h1>
 
-export default App
+      <ChildComponent name={name} changeparentdata={changeparentdata} />
+    </div>
+  );
+};
+
+export default App;
